d7befb56985e2b999d09ca073bbe2ab3a01e6444
NayYanOf <nay.yan@outlook.com>
2025-01-11 01:30:23 -0300

    start adding select on forms

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -3,8 +3,8 @@
 import { useState } from "react";
 import ButtonsContainer from "../components/buttons/ButtonsContainer";
 import Form from "../components/forms/Form";
-import DynamicInput from "../components/inputs/DynamicInput";
 import InputsContainer from "../components/inputs/InputsContainer";
+import DynamicSelect from "../components/inputs/DynamicSelect";
 import { Row, Col } from '../components/flex/flexMounters';
 
 export default function Forms() {
@@ -26,42 +26,69 @@ export default function Forms() {
             <h2>Formulário</h2>
             <div className="flex flex-col gap-4">
                 <Form onSubmit={onSubmit}>
-                    <div className="flex gap-4 p-6 border-solid border-zinc-500 border-[1px] rounded-md">
-                        <InputsContainer
-                            id="firstName"
-                            label="Nome"
-                            placeholder="Insira seu Primeiro Nome"
-                            type="text"
-                            cleanString={true} // FAZER FUNCIONAR PARA OS VALORES RETORNADOS EM DATA DO ON SUBMIT
-                        />
+                    <Row className="flex gap-4 p-6 border-solid border-zinc-500 border-[1px] rounded-md">
+                        <Row>
+                            <Col>
+                                <InputsContainer
+                                    id="firstName"
+                                    label="Nome"
+                                    placeholder="Insira seu Primeiro Nome"
+                                    type="text"
+                                    cleanString={true} // FAZER FUNCIONAR PARA OS VALORES RETORNADOS EM DATA DO ON SUBMIT
+                                />
+                            </Col>
 
-                        <InputsContainer
-                            id="lastName"
-                            label="Sobrenome"
-                            placeholder="Insira seu Sobrenome"
-                            type="text"
-                        />
-                    </div>
-                    
-                    {
-                        submitResult &&
-                        <div className="flex flex-col gap-2 mt-4" >
-                            <span className="text-center font-semibold">Resultado</span>
-                            <div className="flex gap-4 justify-center" >
-                                {
-                                    Object.entries(submitResult).map(([key, value]) => (
-                                        <div key={key}>{`${key}: ${value}`}</div>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    }
+                            <Col>
+                                <InputsContainer
+                                    id="lastName"
+                                    label="Sobrenome"
+                                    placeholder="Insira seu Sobrenome"
+                                    type="text"
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <DynamicSelect
+                                    id="gender"
+                                    label="Gênero"
+                                    placeholder="Selecione seu Gênero"
+                                    options={[
+                                        {label: 'Masculino', value: 'male'},
+                                        {label: 'Feminino', value: 'female'},
+                                        {label: 'Outro', value: 'other'},
+                                    ]}
+                                />
+                            </Col>
+                        </Row>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {
+                                submitResult &&
+                                <div className="flex flex-col gap-2 mt-4" >
+                                    <span className="text-center font-semibold">Resultado</span>
+                                    <div className="flex gap-4 justify-center" >
+                                        {
+                                            Object.entries(submitResult).map(([key, value]) => (
+                                                <div key={key}>{`${key}: ${value}`}</div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            }
+                        </Col>
+                    </Row>
 
-                    <ButtonsContainer
-                        name='Enviar'
-                        className={`${buttonsStyle}`}
-                        variant="submit"
-                    />
+                    <Row>
+                        <Col>
+                            <ButtonsContainer
+                                name='Enviar'
+                                className={`${buttonsStyle}`}
+                                variant="submit"
+                            />
+                        </Col>
+                    </Row>
                 </Form>
 
                 <ButtonsContainer
